fcd706af74993a8049e609b92fe4bd645a4f7254
diegogiacobbo <diegogiacobbo@gmail.com>
2016-06-18 10:47:05 -0300

    - Changement de les folder correctement et cration de les neuveaux beans e views.

---
+++ src/main/java/Controller/LivrosBean.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import java.util.ArrayList;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import Model.Livro;
+
+@ManagedBean
+@ViewScoped
+public class LivrosBean {
+	private ArrayList<Livro> livros;
+
+	public LivrosBean() {
+		this.livros = new ArrayList<>();
+		this.livros.add(new Livro("Java e Orientação a Objetos", "Thiago Faria"));
+		this.livros.add(new Livro("JPA 2 e Hibernate", "Thiago Faria"));
+		this.livros.add(new Livro("JavaServer Faces", "Thiago Faria"));
+		this.livros.add(new Livro("Test Driven Development", "Kent Beck"));
+		this.livros.add(new Livro("Start Small, Stay Small", "Rob Walling"));
+		this.livros.add(new Livro("Trabalhe 4 Horas Por Semana", "Timothy Ferris"));
+		this.livros.add(new Livro("Getting Real", "Jason Fried"));
+		this.livros.add(new Livro("Rework", "Jason Fried"));
+	}
+
+	public ArrayList<Livro> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(ArrayList<Livro> livros) {
+		this.livros = livros;
+	}
+}
--- src/main/java/Controller/OlaBean.java
+++ src/main/java/Controller/OlaBean.java
@@ -1,4 +1,4 @@
-package View;
+package Controller;
 
 import javax.faces.bean.ManagedBean;
 
